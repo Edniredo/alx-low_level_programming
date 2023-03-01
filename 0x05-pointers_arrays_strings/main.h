a999f5a380acf52ac9759679c544fe9e0e976438
Edniredo <adebayoayodeji63@gmail.com>
2023-03-01 08:27:23 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN.H
 #define MAIN.H
 
-#include <stdio.h>
-#include <unistd.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -16,4 +14,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#endf
+#endif
