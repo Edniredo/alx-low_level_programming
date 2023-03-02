3bd53b1b1ee5d138e21b85378e447b97eccce0d9
Edniredo <adebayoayodeji63@gmail.com>
2023-03-02 07:49:04 -0800

    main.h

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,12 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * reset_to_98 - function that takes a pointer to an int as parameter and
- * updates the value it points to to 98.
- *
- * @n: This is the entry
- *
+ * reset_to_98 - writes a function that points to an int and updates
+ *               the value of the address to 98.
+ * @n: The int declared varialble name
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
-}
+}}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndif MAIN.H
-#define MAIN.H
+#ifndef main_h
+#define main_h
 
 
 int _putchar(char c);
